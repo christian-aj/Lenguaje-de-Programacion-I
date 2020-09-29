1e0d83ad08c70cc97a93b8fae6821769ced4eb34
Christian Arevalo <carevalojesus@gmail.com>
2020-09-28 19:52:14 -0500

    index.php

--- Unidad 03/index.php
+++ Unidad 03/index.php
@@ -1,1 +1,37 @@
 <?php
+
+$name = "Christian";
+//echo "hola mundo \t";
+
+$year = array(10,2,5,"true",false);
+
+//echo var_dump($year);
+
+$lastname = "Arevalo";
+
+$a = 20;
+$b = 50;
+$c = $a - $b;
+
+$age = $_POST['age'];
+
+//echo $name . " " . $lastname;
+//echo $c;
+?>
+
+<form action="" method="post">
+
+    <input type="text" placeholder="Enter Age" name="age">
+    <button type="submit">enviar</button>
+
+    <?php
+
+    if ($age >= 18) {
+        echo "<h2>Eres mayor de edad</h2>";
+    } else {
+        echo "todavia eres un chibolo";
+    }
+    ?>
+
+</form>
+<h1>Hola <?php echo $name?></h1>
