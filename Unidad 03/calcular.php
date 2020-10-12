eb0bb3fe836cb5d4ac800db7d24c8de1d0aca947
Christian Arevalo <carevalojesus@gmail.com>
2020-10-12 16:46:23 -0500

    actualizado

--- Unidad 03/calcular.php
+++ Unidad 03/calcular.php
@@ -2,4 +2,16 @@
 
 if (!isset($_POST['enviar'])) {
     header('Location: ejercicio.php');
+} else {
+    $number1 = $_POST['number1'];
+    $number2 = $_POST['number2'];
+    $select = $_POST['calcular'];
+
+
+    switch ($select) {
+        case 1:
+            $resultado = $number1+$number2;
+            echo "El resultado es " . $resultado;
+            break;
+    }
 }
