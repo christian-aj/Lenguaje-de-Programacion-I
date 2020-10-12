3e77110734806fcb2a011e8d778cb5a5a11ccebf
Christian Arevalo <carevalojesus@gmail.com>
2020-10-12 16:30:41 -0500

    clase 03

---
+++ Unidad 03/ejercicio.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ejercicios de PHP</title>
+</head>
+<body>
+<h1>Ejercicios de PHP</h1>
+
+<form action="calcular.php" method="post">
+    <input type="text" placeholder="Ingrese un número">
+    <input type="text" placeholder="Ingrese otro numero">
+    <select name="" id="">
+        <option value="">Sumar</option>
+        <option value="">Restar</option>
+        <option value="">Multiplicar</option>
+        <option value="">Dividir</option>
+    </select>
+    <button type="submit">Enviar</button>
+</form>
+</body>
+</html>
